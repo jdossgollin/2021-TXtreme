a080282c6aa935a410da663a007ccf02128cbc20
James Doss-Gollin <james.dossgollin@gmail.com>
2021-02-21 12:51:53 -0600

    lags are a dimension, not a bunch of variables

--- scripts/era5_return.py
+++ scripts/era5_return.py
@@ -24,29 +24,35 @@ def main() -> None:
     parser.add_argument("-o", "--outfile", type=str)
     args = parser.parse_args()
 
-    era5 = xr.open_mfdataset(args.infiles)["HDD"].compute()
-    rolling = xr.Dataset(
-        {f"hdd_{dur}_days": era5.rolling(time=dur).mean() for dur in DURATIONS}
+    hdd = xr.open_mfdataset(args.infiles)["HDD"].compute()
+    hdd_roll = xr.concat(
+        [hdd.rolling(time=dur).mean().assign_coords({"lag": dur}) for dur in DURATIONS],
+        dim="lag",
     )
 
-    year_eff = pd.to_datetime(rolling["time"].values).year + np.int_(
-        pd.to_datetime(rolling["time"].values).month > 7
+    # group together December YYYY and Jan YYYY+1
+    times = pd.to_datetime(hdd_roll["time"].values)
+    year_eff = times.year + np.int_(times.month > 7)
+    hdd_roll["year_eff"] = xr.DataArray(
+        year_eff, coords={"time": hdd_roll["time"]}, dims="time"
     )
-    rolling["year_eff"] = xr.DataArray(
-        year_eff, coords={"time": rolling["time"]}, dims="time"
-    )
-    annual = rolling.groupby("year_eff").max(dim="time")
 
+    # get ann max heating degree days
+    annual = hdd_roll.groupby("year_eff").max(dim="time")
+
+    # computer return periods
+    # there may be a better way to do this, but this works
     era5_return = annual.sel(year_eff=2021).copy() * 0
     for lon in tqdm(era5_return["longitude"].values):
         for lat in era5_return["latitude"].values:
-            for dur in DURATIONS:
-                var = f"hdd_{dur}_days"
-                sub = annual[var].sel(longitude=lon, latitude=lat)
+            for lag in era5_return["lag"].values:
+                sub = annual.sel(longitude=lon, latitude=lat, lag=lag)
                 hdd_obs = sub.sel(year_eff=slice(0, 2020)).values
                 hdd_21 = sub.sel(year_eff=2021).values
                 rt_estimate = return_period(hdd_21, hdd_obs)
-                era5_return[var].loc[dict(longitude=lon, latitude=lat)] = rt_estimate
+                era5_return.loc[
+                    dict(longitude=lon, latitude=lat, lag=lag)
+                ] = rt_estimate
 
     era5_return.to_netcdf(args.outfile, format="NETCDF4")
 
