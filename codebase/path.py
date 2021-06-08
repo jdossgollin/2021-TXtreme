2bfc8c6eb29d543e0ec3c5b14ffc9f029847db23
James Doss-Gollin <jdossgollin@rice.edu>
2021-06-08 12:34:37 -0400

    better code, worse syntax -- try to trigger the action

--- codebase/path.py
+++ codebase/path.py
@@ -6,29 +6,29 @@ https://juliadynamics.github.io/DrWatson.jl/dev/
 """
 
 import os
-
 PARDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 FIGDIR = os.path.join(PARDIR, "fig")
 DATADIR = os.path.join(PARDIR, "data")
 SCRIPTDIR = os.path.join(PARDIR, "scripts")
-
+def ensure_dir(dir):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
 
 def datadir(*args):
     """Get the directory of something in data"""
-    if not os.path.isdir(DATADIR):
-        os.makedirs(DATADIR)
+    ensure_dir(DATADIR)
     return os.path.join(DATADIR, *args)
-
 
 def scriptdir(*args):
     """Get the directory of something in the scripts folder"""
-    if not os.path.isdir(SCRIPTDIR):
-        os.makedirs(SCRIPTDIR)
+    ensure_dir(SCRIPTDIR)
     return os.path.join(SCRIPTDIR, *args)
+
+
+
 
 
 def figdir(*args):
     """Get the directory of a figure"""
-    if not os.path.isdir(FIGDIR):
-        os.makedirs(FIGDIR)
+    ensure_dir(FIGDIR)
     return os.path.join(FIGDIR, *args)
